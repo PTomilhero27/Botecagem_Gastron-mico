6b0792b7fdbd74c9714508b797744ca9a585f8d5
Tomilhero <djtomilhero@gmail.com>
2025-12-19 17:32:06 -0300

    logs

--- app/services/index.ts
+++ app/services/index.ts
@@ -4,11 +4,11 @@ import { VendorPublicListSchema, VendorSelectedListSchema } from "./schema";
 
 export async function fetchVendorsSelected() {
   const url = process.env.NEXT_PUBLIC_SHEETS_API_CSV_URL!;
+  console.log(url)
   return fetchFromSheetsCsv(url, VendorSelectedListSchema);
 }
 
 export async function fetchVendorsInterested() {
   const url = process.env.NEXT_PUBLIC_SHEETS_CSV_URL!;
-  console.log(url)
   return fetchFromSheetsCsv(url, VendorPublicListSchema); 
 }
